73c780a95a31ca48c5abd4729c0db8e495e64c15
shafran <shafran.andriy@gmail.com>
2025-05-24 16:19:36 +0300

    Created serializer and all functions

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import io
+
 from car.models import Car
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(instance=car)
+    return JSONRenderer().render(serializer.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    return JSONParser().parse(stream)
+
