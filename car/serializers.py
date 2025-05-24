774f12288bb885739ebf3e64e5d3cb9a794c477d
shafran <shafran.andriy@gmail.com>
2025-05-24 16:41:19 +0300

    Created serializer and all functions

--- car/serializers.py
+++ car/serializers.py
@@ -3,10 +3,10 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField(required=False)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField()
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1999)
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(required=False)
 
